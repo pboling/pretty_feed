7593e11b307243c6c971f71371de834a15206c2a
Peter Boling <peter.boling@gmail.com>
2022-03-21 17:07:30 -0700

    ✨ Coverage tracking with simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,12 +20,16 @@ end
 
 COMPAT_LIB = compat_lib
 DEBUG = ENV["DEBUG"] == "true"
-RUN_COVERAGE = ENV["CI_CODECOV"] || ENV["CI"].nil?
 
 ruby_version = Gem::Version.new(RUBY_VERSION)
 minimum_version = ->(version, engine = "ruby") { ruby_version >= Gem::Version.new(version) && RUBY_ENGINE == engine }
-coverage = minimum_version.call("2.7") && RUN_COVERAGE
+actual_version = lambda do |major, minor|
+  actual = Gem::Version.new(ruby_version)
+  major == actual.segments[0] && minor == actual.segments[1] && RUBY_ENGINE == "ruby"
+end
 debugging = minimum_version.call("2.7") && DEBUG
+RUN_COVERAGE = minimum_version.call("2.6") && (ENV['COVER_ALL'] || ENV["CI_CODECOV"] || ENV["CI"].nil?)
+ALL_FORMATTERS = actual_version.call(2, 7) && (ENV['COVER_ALL'] || ENV["CI_CODECOV"] || ENV['CI'])
 
 if DEBUG
   if debugging
@@ -35,7 +39,7 @@ if DEBUG
   end
 end
 
-require "simplecov" if coverage
+require "simplecov" if RUN_COVERAGE
 
 # This gem
 require "pretty_feed"
